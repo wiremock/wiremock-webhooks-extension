9b788d9798bf3f471e05c0b29c386164368fdcd9
Tom Akehurst <t.m.akehurst@googlemail.com>
2016-10-06 15:09:45 +0100

    Moved basic webhook implementation from wiremock branch

---
+++ src/main/java/org/wiremock/webhooks/Webhooks.java
@@ -0,0 +1,93 @@
+package org.wiremock.webhooks;
+
+import com.github.tomakehurst.wiremock.common.Notifier;
+import com.github.tomakehurst.wiremock.core.Admin;
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.extension.PostServeAction;
+import com.github.tomakehurst.wiremock.http.HttpClientFactory;
+import com.github.tomakehurst.wiremock.http.HttpHeader;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static com.github.tomakehurst.wiremock.common.Exceptions.throwUnchecked;
+import static com.github.tomakehurst.wiremock.common.LocalNotifier.notifier;
+import static com.github.tomakehurst.wiremock.http.HttpClientFactory.getHttpRequestFor;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class Webhooks extends PostServeAction {
+
+    private final ScheduledExecutorService scheduler;
+    private final HttpClient httpClient;
+
+    public Webhooks() {
+        scheduler = Executors.newScheduledThreadPool(10);
+        httpClient = HttpClientFactory.createClient();
+    }
+
+    @Override
+    public String getName() {
+        return "webhook";
+    }
+
+    @Override
+    public void doAction(ServeEvent serveEvent, Admin admin, Parameters parameters) {
+        final WebhookDefinition definition = parameters.as(WebhookDefinition.class);
+        final Notifier notifier = notifier();
+
+        scheduler.schedule(
+            new Runnable() {
+                @Override
+                public void run() {
+                    HttpUriRequest request = buildRequest(definition);
+
+                    try {
+                        HttpResponse response = httpClient.execute(request);
+                        notifier.info(
+                            String.format("Webhook %s request to %s returned status %s\n\n%s",
+                                definition.getMethod(),
+                                definition.getUrl(),
+                                response.getStatusLine(),
+                                EntityUtils.toString(response.getEntity())
+                            )
+                        );
+                    } catch (IOException e) {
+                        throwUnchecked(e);
+                    }
+                }
+            },
+            0L,
+            SECONDS
+        );
+    }
+
+    private static HttpUriRequest buildRequest(WebhookDefinition definition) {
+        HttpUriRequest request = getHttpRequestFor(
+                definition.getMethod(),
+                definition.getUrl().toString()
+        );
+
+        for (HttpHeader header: definition.getHeaders().all()) {
+            request.addHeader(header.key(), header.firstValue());
+        }
+
+        if (definition.getMethod().hasEntity()) {
+            HttpEntityEnclosingRequestBase entityRequest = (HttpEntityEnclosingRequestBase) request;
+            entityRequest.setEntity(new ByteArrayEntity(definition.getBinaryBody()));
+        }
+
+        return request;
+    }
+
+    public static WebhookDefinition webhook() {
+        return new WebhookDefinition();
+    }
+}
---
+++ src/test/java/functional/WebhooksAcceptanceTest.java
@@ -0,0 +1,123 @@
+package functional;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.RequestListener;
+import com.github.tomakehurst.wiremock.http.Response;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.apache.http.entity.StringEntity;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.wiremock.webhooks.Webhooks;
+import testsupport.TestNotifier;
+import testsupport.WireMockTestClient;
+
+import java.util.concurrent.CountDownLatch;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.github.tomakehurst.wiremock.http.RequestMethod.GET;
+import static com.github.tomakehurst.wiremock.http.RequestMethod.POST;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.apache.http.entity.ContentType.TEXT_PLAIN;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+import static org.wiremock.webhooks.Webhooks.webhook;
+
+public class WebhooksAcceptanceTest {
+
+    @Rule
+    public WireMockRule targetServer = new WireMockRule(options().dynamicPort());
+
+    CountDownLatch latch;
+    Webhooks webhooks = new Webhooks();
+    TestNotifier notifier = new TestNotifier();
+    WireMockTestClient client;
+
+    @Rule
+    public WireMockRule rule = new WireMockRule(
+        options()
+            .dynamicPort()
+            .notifier(notifier)
+            .extensions(webhooks));
+
+
+    @Before
+    public void init() {
+        targetServer.addMockServiceRequestListener(new RequestListener() {
+            @Override
+            public void requestReceived(Request request, Response response) {
+                if (request.getUrl().startsWith("/callback")) {
+                    latch.countDown();
+                }
+            }
+        });
+        reset();
+        notifier.reset();
+        targetServer.stubFor(any(anyUrl())
+            .willReturn(aResponse().withStatus(200)));
+        latch = new CountDownLatch(1);
+        client = new WireMockTestClient(rule.port());
+        WireMock.configureFor(targetServer.port());
+
+        System.out.println("Target server port: " + targetServer.port());
+        System.out.println("Under test server port: " + rule.port());
+    }
+
+    @Test
+    public void firesASingleWebhookWhenRequested() throws Exception {
+        rule.stubFor(post(urlPathEqualTo("/something-async"))
+            .willReturn(aResponse().withStatus(200))
+            .withPostServeAction("webhook", webhook()
+                .withMethod(POST)
+                .withUrl("http://localhost:" + targetServer.port() + "/callback")
+                .withHeader("Content-Type", "application/json")
+                .withBody("{ \"result\": \"SUCCESS\" }"))
+        );
+
+        verify(0, postRequestedFor(anyUrl()));
+
+        client.post("/something-async", new StringEntity("", TEXT_PLAIN));
+
+        waitForRequestToTargetServer();
+
+        verify(1, postRequestedFor(urlEqualTo("/callback"))
+            .withHeader("Content-Type", equalTo("application/json"))
+            .withRequestBody(equalToJson("{ \"result\": \"SUCCESS\" }"))
+        );
+
+        assertThat(notifier.getInfoMessages(), hasItem(allOf(
+            containsString("Webhook POST request to"),
+            containsString("/callback returned status"),
+            containsString("200")
+        )));
+    }
+
+    @Test
+    public void firesMinimalWebhook() throws Exception {
+        rule.stubFor(post(urlPathEqualTo("/something-async"))
+            .willReturn(aResponse().withStatus(200))
+            .withPostServeAction("webhook", webhook()
+                .withMethod(GET)
+                .withUrl("http://localhost:" + targetServer.port() + "/callback"))
+        );
+
+        verify(0, postRequestedFor(anyUrl()));
+
+        client.post("/something-async", new StringEntity("", TEXT_PLAIN));
+
+        waitForRequestToTargetServer();
+
+        verify(1, getRequestedFor(urlEqualTo("/callback")));
+    }
+
+    private void waitForRequestToTargetServer() throws Exception {
+        latch.await(2, SECONDS);
+        assertThat("Timed out waiting for target server to receive a request",
+            latch.getCount(), is(0L));
+    }
+
+}
